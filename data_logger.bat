0a8c907d356314927527407903b6db5247d10168
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-01-09 04:20:31 -0500

    added backup of python path env variable in Linux start stript

--- data_logger.bat
+++ data_logger.bat
@@ -1,5 +1,7 @@
 set FLASK_APP=run.py
 #set FLASK_ENV=development
 set FLASK_ENV=development
+set PREV_PYTHONPATH = %PYTHONPATH%
 set PYTHONPATH=%PYTHONPATH%;.\
 flask run
+set PYTHONPATH=%PREV_PYTHONPATH%
--- data_logger.sh
+++ data_logger.sh
@@ -1,2 +1,5 @@
+export PREV_PYTHONPATH=$PYTHONPATH
+export $PYTHONPATH=$PYTHONPATH;.\
 export FLASK_APP=run.py
 flask run
+export $PYTHON=$PREV_PYTHONPATH
