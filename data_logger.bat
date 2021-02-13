88652350bda474c0c806ca52b93b1d14cd248ca8
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-02-13 12:15:50 -0500

    Update data_logger.bat

--- data_logger.bat
+++ data_logger.bat
@@ -2,6 +2,6 @@ set FLASK_APP=run.py
 #set FLASK_ENV=development
 set FLASK_ENV=development
 set PREV_PYTHONPATH = %PYTHONPATH%
-set PYTHONPATH=%PYTHONPATH%;.\
+set PYTHONPATH=%PYTHONPATH%;.\;.
 flask run
 set PYTHONPATH=%PREV_PYTHONPATH%
