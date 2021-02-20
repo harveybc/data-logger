c7bfae89726f61ffc0fa9ff2b6ac0a5661881608
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-02-20 14:11:29 -0500

    updated startup scripts

--- data_logger.bat
+++ data_logger.bat
@@ -1,5 +1,4 @@
 set FLASK_APP=run.py
-#set FLASK_ENV=development
 set FLASK_ENV=development
 set PREV_PYTHONPATH = %PYTHONPATH%
 set PYTHONPATH=%PYTHONPATH%;.\;\controllers
--- data_logger.sh
+++ data_logger.sh
@@ -1,5 +1,6 @@
-export PREV_PYTHONPATH=$PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:./
 export FLASK_APP=run.py
+export FLASK_ENV=development
+export PREV_PYTHONPATH=$PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:./:/controllers
 flask run
 export PYTHONPATH=$PREV_PYTHONPATH
