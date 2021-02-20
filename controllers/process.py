a309b4dca395705ab71a5f9e16363c8cfd48fda9
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-02-20 15:34:12 -0500

    added controllers

---
+++ controllers/process.py
@@ -0,0 +1,59 @@
+""" Handlers for the user endpoint  """
+
+def create():
+    """ Parse command line parameters.
+
+        Args:
+        args ([str]): command line parameters as list of strings
+
+        Returns:
+        :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    #return 'You created the user id='+id+', username='+username+', email='+email+', password='+password+', is_admin='+is_admin 
+
+def get():
+    """ Parse command line parameters.
+                  
+        Args:
+        args ([str]): command line parameters as list of strings
+
+        Returns:
+        :obj:`argparse.Namespace`: command line parameters namespace
+    """
+    return 'id='+id+', username='+username+', email='+email+', password='+password+', is_admin='+is_admin 
+
+def update():
+    """ Parse command line parameters.
+
+        Args:
+        args ([str]): command line parameters as list of strings
+
+        Returns:
+        :obj:`argparse.Namespace`: command line parameters namespace
+    """
+
+def delete():
+    """ Parse command line parameters.
+
+        Args:
+        args ([str]): command line parameters as list of strings
+
+        Returns:
+        :obj:`argparse.Namespace`: command line parameters namespace
+    """
+
+def get_list():
+    """ Parse command line parameters.
+
+        Args:
+        args ([str]): command line parameters as list of strings
+
+        Returns:
+        :obj:`argparse.Namespace`: command line parameters namespace
+    """
+
+
+
+
+
+
