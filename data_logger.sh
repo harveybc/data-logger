2aa9e19024689042102dcd9be9e44751f0b6bc0d
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-02-20 15:26:41 -0500

    Update data_logger.sh

--- data_logger.sh
+++ data_logger.sh
@@ -2,5 +2,6 @@ export FLASK_APP=run.py
 export FLASK_ENV=development
 export PREV_PYTHONPATH=$PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:./:/controllers
-flask run
+flask run --host=0.0.0.0
+echo "WARNING!!"
 export PYTHONPATH=$PREV_PYTHONPATH
