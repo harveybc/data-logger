e61883de67f17d88b57ed69590fc80c735087da3
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-04-08 23:13:42 -0500

    updated start script for linux

--- data_logger.sh
+++ data_logger.sh
@@ -2,6 +2,6 @@ export FLASK_APP=run.py
 export FLASK_ENV=development
 export PREV_PYTHONPATH=$PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:./:/controllers
-flask run --host=0.0.0.0
+flask run --host=0.0.0.0 
 echo "WARNING!!"
 export PYTHONPATH=$PREV_PYTHONPATH
