af348d968973ca4b4afb81b614c23fa7644607d4
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-04-11 01:15:58 -0500

    Update user.py

--- controllers/user.py
+++ controllers/user.py
@@ -3,20 +3,36 @@
 from models.user import User
 from app.app import db
 import json
+from sqlalchemy.exc import SQLAlchemyError
 
 def create(body): 
-    """ Parse command line parameters.
+    """ Create a register in db based on a json from a request's body parameter.
 
         Args:
-        args ([str]): command line parameters as list of strings
+        body (dict): dict containing the fields of the new register, obtained from json in the body of the request.
 
         Returns:
-        :obj:`argparse.Namespace`: command line parameters namespace
+        new_user (dict): the newly created user register
     """
+    # instantiate user with the body dict as kwargs
     new_user = User(**body)
+    # create new flask-sqlalchemy session
     db.session.add(new_user)
-    db.session.commit()
-    return 'User was succesfully created'
+    try:
+        db.session.commit()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error
+    # test if the new user was created 
+    try:
+        res = User.query.filter_by(username=new_user.username).first_or_404()
+    except SQLAlchemyError as e:
+        error = str(e.__dict__['orig'])
+        return error
+    # remove pass
+    res.password=""
+    # return register as dict
+    return res.as_dict()
     
 def get(userId):
     """ Parse command line parameters.
