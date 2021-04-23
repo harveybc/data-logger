bf5aa395c5711de124eb1a6a8bb2972b701e297b
harvey <harveybc@ingeni-us.com>
2021-04-23 12:19:44 -0500

    updated user controller

--- controllers/user.py
+++ controllers/user.py
@@ -82,7 +82,7 @@ def update(userId, body):
         return error
     # test if the model was updated 
     try:
-        res = User.query.filter_by(id=int(userId)).first_or_404()
+        res = User.query.filter_by(username=userId).first_or_404()
     except SQLAlchemyError as e:
         error = str(e.__dict__['orig'])
         return error
