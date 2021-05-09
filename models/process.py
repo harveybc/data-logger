3e47e83298cf64e700ea37f5a0ed5e717e9860d2
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-05-09 03:34:44 -0500

    Update process.py

--- models/process.py
+++ models/process.py
@@ -1,6 +1,5 @@
 """ Map this model's fields and relationships """
     
-from app.base.util import hash_pass
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from flask_sqlalchemy import SQLAlchemy
 from app.app import db
@@ -22,34 +21,9 @@ class Process(db.Model):
     # relationships
     user = relationship("User", back_populates='processes')
 
-
-    def __init__(self, **kwargs):
-        for property, value in kwargs.items():
-            # depending on whether value is an iterable or not, we must
-            # unpack it's value (when **kwargs is request.form, some values
-            # will be a 1-element list)
-            if hasattr(value, '__iter__') and not isinstance(value, str) and not isinstance(value, dict):
-                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
-                print("value=", value)
-                value = value[0]
-
-            if property == 'password':
-                value = hash_pass( value ) # we need bytes here (not plain str)
-                
-            setattr(self, property, value)
-
     def __repr__(self):
         return str(self.name)
 
-    def as_dict(self):   
-        r2 = {}
-        for c in self.__table__.columns:
-            attr = getattr(self, c.name)
-            if is_num(attr):
-                r2[c.name]=attr
-            else:
-                r2[c.name]=str(attr)
-        return r2
 
 
 
