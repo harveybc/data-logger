310b79f78cce00d5268b195e23bca85d218c9acc
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-05-09 03:55:23 -0500

    Update process.py

--- controllers/process.py
+++ controllers/process.py
@@ -18,10 +18,9 @@ def create(body):
         res (dict): the newly created process register with empty password field.
     """
     # use kwargs to check if the process, table or register parameters are present
-    body_params = body.__dict__
-    if hasattr(body_params, 'process'):
+    if hasattr(body, 'process'):
         # instantiate process with the body dict as kwargs
-        new_process = Process(body_params.process)
+        new_process = Process(**body.process)
         # create new flask-sqlalchemy session
         
         # TODO: verify if the user is admin or the userid is the same as the requesting user
