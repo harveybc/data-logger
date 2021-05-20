3517fb43265006a8009ddc5683c3a9e7d06f3cca
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-05-20 14:49:45 -0500

    Update process.py

--- controllers/process.py
+++ controllers/process.py
@@ -56,7 +56,7 @@ def create(body):
     if 'table' in body:
         # instantiate process with the body dict as kwargs
         new_table = ProcessTable(**body['table'])
-        if not DefaultDialect.has_table(db, new_table.name):
+        if not DefaultDialect.has_table(DefaultDialect(), db, new_table.name):
             new_table.table.create(db.engine)
         # test if the new process was created 
         try:
