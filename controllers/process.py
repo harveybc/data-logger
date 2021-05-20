001b197586fcd6ee75e2d1f6e6f353ddc6587bef
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-05-20 14:45:15 -0500

    Update process.py

--- controllers/process.py
+++ controllers/process.py
@@ -10,6 +10,7 @@ from datetime import datetime
 from app.app import login_manager
 from models.process import Process
 from models.process_table import ProcessTable
+from sqlalchemy.engine.default import DefaultDialect
 
 @login_required
 def create(body): 
@@ -55,7 +56,7 @@ def create(body):
     if 'table' in body:
         # instantiate process with the body dict as kwargs
         new_table = ProcessTable(**body['table'])
-        if not db.dialect.has_table(db, new_table.name):
+        if not DefaultDialect.has_table(db, new_table.name):
             new_table.table.create(db.engine)
         # test if the new process was created 
         try:
