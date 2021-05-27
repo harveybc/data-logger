0ca5f09431f1437cb0f5da5ec89e43c347b7adf2
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-05-27 00:35:53 -0500

    Update process_table.py

--- controllers/process_table.py
+++ controllers/process_table.py
@@ -1,6 +1,7 @@
 """ Controller for the process table endpoint. 
     Description: Contains API endpoint handler functions for CRUD (create, read, update, delete) and other model operations.  
 """
+from sqlalchemy.ext.automap import automap_base
 
 def read_all():
     try:
