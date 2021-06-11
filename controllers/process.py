465cae2b127af0f6d6677c067ed3a74255e4d527
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-06-11 03:15:20 -0500

    Update process.py

--- controllers/process.py
+++ controllers/process.py
@@ -264,5 +264,22 @@ def read_all():
             register_model = eval("Base.classes." + table_param)
             # perform query
             res=db.session.query(register_model).all()
-            return res.__dict__
+            return [as_dict(c) for c in res]
+
+def as_dict(model):   
+        r2 = {}
+        for c in model.__table__.columns:
+            attr = getattr(model, c.name)
+            if is_num(attr):
+                r2[c.name]=attr
+            else:
+                r2[c.name]=str(attr)
+        return r2
+
+def is_num(n):
+    if isinstance(n, int):
+        return True
+    if isinstance(n, float):
+        return n.is_integer()
+    return False
             
