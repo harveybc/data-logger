c88e26a5edeed115952fafd1b37859f72887b4b2
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-06-17 20:27:06 -0500

    Update update.py

--- controllers/process/update.py
+++ controllers/process/update.py
@@ -27,7 +27,7 @@ def update(processId, body):
     """    
     #initialize void response
     res = {}
-    # use kwargs to check if the process parameter is present
+    # check if the process parameter is present
     if 'process' in body:
         # query the existing register
         try:
@@ -51,43 +51,7 @@ def update(processId, body):
             error = str(e)
             res['process'] = { 'error_a' : error}
         
-    # use kwargs to check if the table parameter is present    
-    if 'table' in body:
-        # instantiate process table with the body dict as kwargs
-        new_table = ProcessTable(**body['table'])
-        if not db.engine.dialect.has_table(db.engine, new_table.name):
-            new_table.table.create(db.engine)
-        #update metadata and tables
-        db.Model.metadata.reflect(bind=db.engine)
-        # reflect the tables
-        Base = automap_base()
-        Base.prepare(db.engine, reflect=True)
-        
-        # add the table to the tables array in the process (convert to string for compatibility)
-        try:
-            p_model = Process.query.filter_by(id=new_table.process_id).first_or_404()
-            p_table = p_model.as_dict()
-            # construct a table model (see swagger yaml) with table_column models
-            table_m = {}
-            table_m["name"] = new_table.name
-            # TODO: verify if its neccesary to have the real_name attribute or of is required to use a prefix
-            table_m["real_name"] = new_table.name
-            table_m["columns"] = new_table.columns
-            # convert the tables string to an array
-            t_array = json.loads(p_table["tables"])
-            #insert the new table model in the tables array
-            t_array.append(table_m)
-            # save the table_m array in a json string in process.tables 
-            p_table["tables"] = json.dumps(t_array)
-            # update the tables attribute in the process model
-            p_model.tables = p_table["tables"] 
-            db.session.commit()
-            db.session.close()
-        except SQLAlchemyError as e:
-            error = str(e)
-            res['process'] ={ 'errorz' : error}
-        # update  the output in case the table was created in the same request as the process
-        if "process" in res:
+    if "process" in res:
             res['process']["tables"] = p_table["tables"]
             
         # test if the new process table  was created 
