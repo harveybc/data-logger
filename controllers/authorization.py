d6cdd962c584391c303d2afd75716d73b080c934
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-06-20 14:52:29 -0500

    Update authorization.py

--- controllers/authorization.py
+++ controllers/authorization.py
@@ -1,4 +1,21 @@
-""" Handlers for the user endpoint  """
+""" Controller for the authorization endpoints. 
+    Description: Contains API endpoint handler functions for CRUD operations 
+    on the authorizations table whose registers are an ordered set of rules
+    that a request must approve to perform its intended action.
+    
+    By default, admin users can CRUD processes, tables and table registers.
+    
+    Also by default, all users can read/create registers on their processes' tables, 
+    but can't create processes or tables, and can't update/delete registers.
+
+    When authorizations are created for an user, process or a table
+    the user is denied all access but the indicated.
+
+    The list of authorizations for an user, proess or table is ordered by the 
+    priority column in ascending order and the highest priority rules override 
+    the lowest priority ones.
+
+"""
 
 def create():
     """ Parse command line parameters.
