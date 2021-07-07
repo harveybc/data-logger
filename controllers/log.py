9b70b4516d3d09c14c75dc9ca401dd2a73fbdb22
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-07-07 01:05:08 -0500

    Update log.py

--- controllers/log.py
+++ controllers/log.py
@@ -221,10 +221,11 @@ def log_request(*args, **kwargs):
     # add the new_log to the session
     db.session.add(new_log)
     try:
-        db.session.flush()
-        new_id = new_log.id
+        #db.session.flush()
+        
         db.session.commit()
         db.session.close()
+        new_id = new_log.id
     except SQLAlchemyError as e:
         error = str(e)
         return -1
