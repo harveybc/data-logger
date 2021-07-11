7e3d316d9f7060827b002f2f3c46d6535173e71a
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-07-10 20:42:50 -0500

    updated plugin system

--- run.py
+++ run.py
@@ -1,13 +1,29 @@
 # -*- encoding: utf-8 -*-
-
+""" DataLogger: Run script.
+    Description: Initializes the plugin system and starts the application.
+    This script is called by the command 'flask run' in the startup scripts 'data_logger.sh' and 'data_logger.bat', 
+    where FLASK_APP env variable is set to 'run.py'(this script).
+"""
 
 from flask_migrate import Migrate
 from os import environ
 from sys import exit
 from decouple import config
-
+from json import loads
 from config import config_dict
 from app.app import create_app, db
+from app.data_logger import DataLogger
+
+# load the plugin config file from /plugin_config.json
+print("DataLogger: Loading plugin configuration from /plugin_config.json ")
+try:
+    with open("plugin_config.json", "r") as conf_file:
+        plugin_conf = json.load(conf_file)
+except:
+    exit("Can't load plugin configuration from /plugin_config.json")
+# initialize plugin system
+data_logger_instance = DataLogger(plugin_conf)
+
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True)
@@ -16,7 +32,6 @@ DEBUG = config('DEBUG', default=True)
 get_config_mode = 'Debug' if DEBUG else 'Production'
 
 try:
-    
     # Load the configuration using the default values 
     app_config = config_dict[get_config_mode.capitalize()]
 
