23f10f42d7cf4286eb745cd807cdd44631d104d0
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-07-16 02:02:12 -0500

    Update run.py

--- run.py
+++ run.py
@@ -9,8 +9,7 @@ from flask_migrate import Migrate
 from os import environ
 from sys import exit
 from decouple import config
-from json import loads
-from config import config_dict
+from json import load as json_load
 from app.app import create_app, db
 from app.data_logger import DataLogger
 
@@ -18,7 +17,7 @@ from app.data_logger import DataLogger
 print("Loading plugin configuration from /plugin_config.json ")
 try:
     with open("plugin_config.json", "r") as conf_file:
-        plugin_conf = json.load(conf_file)
+        plugin_conf = json_load(conf_file)
 except:
     exit("Can't load plugin configuration from /plugin_config.json")
 # initialize plugin system
@@ -27,6 +26,9 @@ data_logger_instance = DataLogger(plugin_conf)
 DEBUG = config('DEBUG', default=True)
 # setup config mode
 get_config_mode = 'Debug' if DEBUG else 'Production'
+# TODO: USE STORE PLUGIN EP?
+from config import config_dict
+# load config from the config_dict according to the set config mode.
 try:
     # Load the configuration using the default values 
     app_config = config_dict[get_config_mode.capitalize()]
