dbf2ace42e4fb11417b74dc01cfa6b567b6e2e80
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-07-18 02:37:45 -0500

    Update data_logger.py

--- app/data_logger.py
+++ app/data_logger.py
@@ -26,49 +26,45 @@ class DataLogger(DataLoggerBase):
         store_conf (JSON): store plugin configuration
         core_conf (JSON): core plugin configuration
         gui_conf (JSON): gui plugin configuration
-        """
-        self.conf = conf
-        if conf != None:         
-            self.setup_logging(logging.DEBUG) 
-            _logger.debug(" * Creating data_logger instance...")
-            # list available plugins
-            if 'list_plugins' in conf:
-                if self.conf['list_plugins'] == True:
-                    _logger.debug("Listing plugins.")
-                    self.find_plugins()
-                    _logger.debug("Printing plugins.")
-                    self.print_plugins()
-            # set default plugin values if not found on config and execute core operations
+        """         
+        self.setup_logging(logging.DEBUG) 
+        # list available plugins
+        if ('list_plugins' in store_conf) or ('list_plugins' in core_conf) or ('list_plugins' in gui_conf):
+            if self.conf['list_plugins'] == True:
+                _logger.debug("Finding available plugins.")
+                self.find_plugins()
+                _logger.debug("Printing available plugins.")
+                self.print_plugins()
+        # set default plugin values if not found on config and execute core operations
+        else:
+            # sets default values for plugins
+            if 'core_plugin' not in core_conf: 
+                self.conf['core_plugin'] = "core_basic_auth"
+                _logger.debug("Warning: core plugin not found in config file, using core_basic_auth")
+            if 'store_plugin' not in store_conf: 
+                self.conf['store_plugin'] = "store_sqlite"  
+                _logger.debug("Warning: store plugin not found, using store_sqlite")
+            if 'gui_plugin' not in gui_conf: 
+                self.conf['gui_plugin'] = "gui_basic_auth"
+                _logger.debug("* Warning: gui plugin not found, using core_basic_auth")
+            _logger.debug("* Finding Plugins.")
+            self.find_plugins()
+            _logger.debug("* Loading plugins.")
+            self.load_plugins() 
+            if self.core_conf['core_plugin'] != None:
+                _logger.debug("Setting up store plugin" )
+                self.input_ds = self.ep_input.load_data() 
+                _logger.debug("Performing core operations from the  core plugin.")
+                self.output_ds = self.ep_core.core(self.input_ds) 
+                _logger.debug("Executing the output plugin.")
+                self.ep_output.store_data(self.output_ds) 
+                _logger.info("feature_extractor finished.")
             else:
-                # sets default values for plugins
-                if 'core_plugin' not in conf: 
-                    self.conf['core_plugin'] = "core_basic_auth"
-                    _logger.debug("Warning: core plugin not found in config file, using core_basic_auth")
-                if 'store_plugin' not in conf: 
-                    self.conf['store_plugin'] = "store_sqlite"  
-                    _logger.debug("Warning: store plugin not found, using store_sqlite")
-                if 'gui_plugin' not in conf: 
-                    self.conf['gui_plugin'] = None
-                    _logger.debug("* Warning: gui plugin not found")
-                # execute core operations
-                self.core()
+                exit('Error: No core plugin loaded.')
 
     def core(self):
-        """ Core feature_extractor operations. """
-        _logger.debug("* Finding Plugins.")
-        self.find_plugins()
-        _logger.debug("* Loading plugins.")
-        self.load_plugins() 
-        if self.conf['core_plugin'] != None:
-            _logger.debug("Setting up store plugin" )
-            self.input_ds = self.ep_input.load_data() 
-            _logger.debug("Performing core operations from the  core plugin.")
-            self.output_ds = self.ep_core.core(self.input_ds) 
-            _logger.debug("Executing the output plugin.")
-            self.ep_output.store_data(self.output_ds) 
-            _logger.info("feature_extractor finished.")
-        else:
-            exit('Error: No core plugin loaded.')
+        """ Core data_logger operations. """
+        
             
     
     def setup_logging(self, loglevel):
