22651d9cc2b449ec2ac7d449a16e60273a801e3e
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-07-18 19:31:28 -0500

    Update __init__.py

--- plugins/store/sqlite_store/__init__.py
+++ plugins/store/sqlite_store/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-This File contains the LoadCSV class plugin. 
+This File contains the SqliteStore class plugin that allows data_logger to use a sqlite database. 
 """
 
 import json
+from .config import config_dict
 
 __author__ = "Harvey Bastidas"
 __copyright__ = "Harvey Bastidas"
@@ -13,14 +14,13 @@ class SqliteStore():
     """ input plugin for the FeatureExtractor class, after initialization, the input_ds attribute is set """
 
     def __init__(self, conf):
-        """ Initializes PluginBase. Do NOT delete the following line whether you have initialization code or not. """
-        super().__init__(conf)
+        """ assign configuration params as class attributes """
         # Insert your plugin initialization code here.
-        pass
+        self.conf = conf
         
     def get_config_dict(self):
         """  Returns the config dict from this plugin's config.py """
-        pass
+        return config_dict
     
     #Imported methods
     #from ._dashboard import load_data, get_user_id, get_max, get_count, get_column_by_pid, get_columns, get_users, get_user_by_username, get_processes, get_process_by_pid, processes_by_uid
