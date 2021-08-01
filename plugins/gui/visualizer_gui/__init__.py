04071e1912ef2b0ceef2108836c7e0dc3c98236a
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-07-31 23:26:33 -0500

    Update __init__.py

--- plugins/gui/visualizer_gui/__init__.py
+++ plugins/gui/visualizer_gui/__init__.py
@@ -22,12 +22,12 @@ class VisualizerGui():
         _logger.info("Initializing VisualizerGui plugin")
         self.conf = conf
         # rel_path = os.path.relpath(os.path.dirname(__file__) )
-        print("abspath=", os.path.abspath(__file__) +'/base/static')
+        #print("abspath=", os.path.abspath(__file__) +'/base/static')
         #print("commonpath=", os.path.commonpath(__file__))
-        print("basename=", os.path.basename(__file__) +'/base/static')
-        print("relpath=", os.path.relpath(os.curdir, start=os.curdir))
+        #print("basename=", os.path.basename(__file__) +'/base/static')
+        #print("relpath=", os.path.relpath(os.curdir, start=os.curdir))
         # path for static files .//base/static
-        self.static_url_path = os.path.abspath(__file__)+'/base/static'
+        self.static_url_path = os.path.abspath(__file__)+'plugins/gui/visualizer_gui/base/static'
         #self.specification_dir = os.path.dirname(__file__)
         #self.specification_filename = 'DataLogger-OAS.apic.yaml'
         #self.User = User
