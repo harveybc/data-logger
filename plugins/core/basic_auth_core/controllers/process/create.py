d2f951d3d9560044a3be8de22ea3d74c0738ff8e
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-08-22 15:35:37 -0500

    Update create.py

--- plugins/core/basic_auth_core/controllers/process/create.py
+++ plugins/core/basic_auth_core/controllers/process/create.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from app.app import login_manager
 from ...models.process import Process
 from ...models.process_table import ProcessTable
-from ...models.process_register_factory import ProcessRegisterFactoryFactory
+from ...models.process_register_factory import ProcessRegisterFactory
 from sqlalchemy.ext.automap import automap_base
 from ...controllers.common import as_dict, is_num
 from ...controllers.authorization import authorization_required
