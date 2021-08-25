2da040059dcdf7406ed167ff33729dfb8c98fc50
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-08-25 13:43:44 -0500

    Update create.py

--- plugins/core/basic_auth_core/controllers/process/create.py
+++ plugins/core/basic_auth_core/controllers/process/create.py
@@ -76,5 +76,5 @@ def create(body):
         register_model = ProcessRegisterFactory(body['register']['table'])    
         register_instance = register_model.create(**body['register'])    
         # return register as dict
-        res['register'] = as_dict(register_instance)
+        res['register'] = register_instance
     return res
