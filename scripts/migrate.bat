25e15091a607fd73e81f6500f5c832899e820b98
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-11-05 18:30:49 -0500

    Update migrate.bat

--- scripts/migrate.bat
+++ scripts/migrate.bat
@@ -1,8 +1,8 @@
 echo "This script is made to be executed from the data_logger's root directory."
-set PREV_PYTHONPATH = %PYTHONPATH%
-set PYTHONPATH=%PYTHONPATH%;.\;\controllers;.
+#set PREV_PYTHONPATH = %PYTHONPATH%
+#set PYTHONPATH=%PYTHONPATH%;.\;\controllers;.
 set FLASK_APP=.\app\migrate.py
 set FLASK_ENV=development
-flask run 
-set PYTHONPATH=%PREV_PYTHONPATH%
+flask dbinit 
+#set PYTHONPATH=%PREV_PYTHONPATH%
  
