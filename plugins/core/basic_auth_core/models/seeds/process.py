babd9e7ac03cfec0b8a259da200ed742a9fdbb45
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-11-06 03:54:16 -0500

    test

--- plugins/core/basic_auth_core/models/seeds/process.py
+++ plugins/core/basic_auth_core/models/seeds/process.py
@@ -2,6 +2,6 @@ from ...models.process import Process
 # add the default process with id = 0
 def seed(app, db):
     with app.app_context():
-        tmp =  Process(id=0, name='default', description="default process", tables="[]", user_id=0)
+        tmp =  Process(name='default', description="default process", tables="[]", user_id=0)
         db.session.add(tmp)
         db.session.commit()
--- plugins/core/basic_auth_core/models/seeds/user.py
+++ plugins/core/basic_auth_core/models/seeds/user.py
@@ -2,6 +2,6 @@ from ...models.user import User
 # add the default user with id = 0 and username = test
 def seed(app, db):
     with app.app_context():
-        tmp =  User(id=0, username='test', password='pass', admin=True, email='test@test.com')
+        tmp =  User( username='test', password='pass', admin=True, email='test@test.com')
         db.session.add(tmp)
         db.session.commit()
