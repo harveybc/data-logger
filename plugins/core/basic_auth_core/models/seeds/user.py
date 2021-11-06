18a2ec3f6f8d5ca0d4bff0d05a46deb1133f3ae4
Harvey Bastidas <harveybc@users.noreply.github.com>
2021-11-06 04:14:57 -0500

    Update user.py

--- plugins/core/basic_auth_core/models/seeds/user.py
+++ plugins/core/basic_auth_core/models/seeds/user.py
@@ -3,6 +3,7 @@ import logging
 _logger = logging.getLogger(__name__)
 # add the default user with id = 0 and username = test
 def seed(app, db):
+    _logger.info(" Seeding user table..")
     with app.app_context():
         tmp =  User( username='test', password='pass', admin=True, email='test@test.com')
         db.session.add(tmp)
