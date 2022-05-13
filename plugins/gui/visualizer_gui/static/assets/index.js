05b6e689404d899415c53aa98a9af05cf7d0f97c
Harvey Bastidas <harveybc@ingeni-us.com>
2022-05-13 02:17:30 -0500

    updated vue module structure

--- plugins/gui/visualizer_gui/static/assets/index.js
+++ plugins/gui/visualizer_gui/static/assets/index.js
@@ -2,23 +2,24 @@
 export default function() {
     return({    
         data() {
-            return { field_start_values }
+            return { 
+                count: 0
+            }
           },
           methods: {
-              increment() {
-                this.count++
-              }
+                increment() {
+                    this.count++
+                },
+                // define starting field values
+                field_start_values(){
+                    return {
+                        count:0
+                    }
+                }
           },
           compilerOptions: {
               // Initialize vue to have different delimiters to the ones used by jinja in flask: [[ , ]] 
               delimiters: ["[[", "]]"]
           }
     })
-}
-
-// define starting field values
-function field_start_values(){
-    return {
-        count:0
-    }
 }
