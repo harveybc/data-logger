ec3bae52ea75221fdd9f34f680292a039dd784b3
Harvey Bastidas <harveybc@ingeni-us.com>
2022-05-13 02:31:11 -0500

    updated vue mudule structure

--- plugins/gui/visualizer_gui/static/assets/index.js
+++ plugins/gui/visualizer_gui/static/assets/index.js
@@ -1,23 +1,25 @@
 // dashboard vue Module implementation
-export default {
-    data() {
-        return { 
-            count: 0
-        }
-    },
-    methods: {
-        increment() {
-            this.count++
-        },
-        // define starting field values
-        field_start_values(){
-                return {
-                count:0
+export function MyModule() { 
+    return({
+        data() {
+            return { 
+                count: 0
             }
+        },
+        methods: {
+            increment() {
+                this.count++
+            },
+            // define starting field values
+            field_start_values(){
+                    return {
+                    count:0
+                }
+            }
+        },
+        compilerOptions: {
+            // Initialize vue to have different delimiters to the ones used by jinja in flask: [[ , ]] 
+            delimiters: ["[[", "]]"]
         }
-    },
-    compilerOptions: {
-        // Initialize vue to have different delimiters to the ones used by jinja in flask: [[ , ]] 
-        delimiters: ["[[", "]]"]
-    }
+    })
 }
