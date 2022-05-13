d70e484a9d6a78eee82b7e7a8cfcedc64c623854
Harvey Bastidas <harveybc@ingeni-us.com>
2022-05-13 01:50:41 -0500

    Update index.js

--- plugins/gui/visualizer_gui/static/assets/index.js
+++ plugins/gui/visualizer_gui/static/assets/index.js
@@ -1,19 +1,17 @@
 // dashboard vue Module implementation
-function Module() {
-    return({    
-        data() {
-            return { field_start_values }
-          },
-          methods: {
-              increment() {
-                this.count++
-              }
-          },
-          compilerOptions: {
-              // Initialize vue to have different delimiters to the ones used by jinja in flask: [[ , ]] 
-              delimiters: ["[[", "]]"]
-          }
-    })
+export default {
+    data() {
+      return { field_start_values }
+    },
+    methods: {
+        increment() {
+          this.count++
+        }
+    },
+    compilerOptions: {
+        // Initialize vue to have different delimiters to the ones used by jinja in flask: [[ , ]] 
+        delimiters: ["[[", "]]"]
+    }
 }
 
 // define starting field values
