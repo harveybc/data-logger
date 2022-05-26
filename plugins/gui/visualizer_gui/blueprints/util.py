1b384c21909b7ed508cf3454ac2c493c3f1521fd
Harvey Bastidas <harveybc@ingeni-us.com>
2022-05-26 00:15:29 -0500

    Update util.py

--- plugins/gui/visualizer_gui/blueprints/util.py
+++ plugins/gui/visualizer_gui/blueprints/util.py
@@ -21,7 +21,7 @@ def new_bp(plugin_folder, core_ep):
     # construct the data_logger blueprint using the plugin folder as template folder
     bp = Blueprint("util_bp", __name__, template_folder=plugin_folder+"/templates")
     
-    @bp.route("/")
+    @bp.route("/column_max")
     #@login_required
     def index():
         # read the data to be visualized using the using the Feature extractor instance, preinitialized in __init__.py with input and output plugins entry points.
