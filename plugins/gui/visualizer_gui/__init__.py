b17ed63f54ae6a7de12917620f7568a959804fd6
Harvey Bastidas <harveybc@ingeni-us.com>
2022-05-26 00:06:34 -0500

    Update __init__.py

--- plugins/gui/visualizer_gui/__init__.py
+++ plugins/gui/visualizer_gui/__init__.py
@@ -27,7 +27,7 @@ class VisualizerGui():
     # register blueprints for gui    
     def register_blueprints(self, app, core_ep):
         """ create the blueprints with all routes of the gui """
-        for module_name in ('base', 'dashboard', 'user', 'process'):
+        for module_name in ('base', 'dashboard', 'user', 'process', 'util'):
             module = import_module('plugins.gui.visualizer_gui.blueprints.{}'.format(module_name))
             bp = module.new_bp(self.template_path(), core_ep)
             app.register_blueprint(bp)
