6d417d8aa4bb990c24b3c77fe019685628b8833a
Harvey Bastidas <harveybc@ingeni-us.com>
2022-05-31 03:08:55 -0500

    Update util.py

--- plugins/gui/visualizer_gui/blueprints/util.py
+++ plugins/gui/visualizer_gui/blueprints/util.py
@@ -26,7 +26,7 @@ def new_bp(plugin_folder, core_ep, store_ep):
     def column_max():
         table = request.args.get('table')
         column = request.args.get('column')
-        results = core_ep.column_max(table, column)
+        results = core_ep.column_max(get_db(), table, column)
         return results
 
 #    @bp.route("/<int:pid>/trainingpoints")
