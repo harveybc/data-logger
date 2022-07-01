0ece8400ae11e41b87b9ecfb5521a657daaa35af
Harvey Bastidas <harveybc@ingeni-us.com>
2022-07-01 18:42:10 -0500

    Update index.js

--- plugins/gui/visualizer_gui/static/assets/index.js
+++ plugins/gui/visualizer_gui/static/assets/index.js
@@ -10,34 +10,52 @@ export default {
     }, 
     methods: {
         // returns the fe_config.id for the minimum fe_training_error.mse for the current process
-        max_training_mse(){},
+        max_training_mse(){
+            // use the response of api request
+            axios.get('/max_training_mse', {
+              params :{}
+            })
+            .then((response) => {
+              return response;
+            }, (error) => {
+              console.log(error);
+              return 0;
+            });        
+        },
         // returns the fe_config.id for the minimum fe_training_error.mse for the current process
-        max_validation_mse(){},
-
-
-
-
+        max_validation_mse(){
+          // use the response of api request
+          axios.get('/max_validation_mse', {
+            params :{}
+          })
+          .then((response) => {
+            return response;
+          }, (error) => {
+            console.log(error);
+            return 0;
+          });
+        },
+        
         // returns the máximum value for a given table and column for the processes of the current user
         user_column_max(username, table, column) {
-
             // use the result of api request
             axios.get('/column_max', {
-                //table: table,
-                //column: column
-                params :{
-                  table: 'fe_training_error',
-                  column: 'mse'
-                }
-                
-              })
-              .then((response) => {
-                return response;
-                //console.log(response);
-              }, (error) => {
-                console.log(error);
-                return 0;
-              });
-            
+              //table: table,
+              //column: column
+              params :{
+                table: 'fe_training_error',
+                column: 'mse'
+              }
+              
+            })
+            .then((response) => {
+              return response;
+              //console.log(response);
+            }, (error) => {
+              console.log(error);
+              return 0;
+            });
+          
         },
         // returns the number of users for the processes of the user
         process_count_users() {
