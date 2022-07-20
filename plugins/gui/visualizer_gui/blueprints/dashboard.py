46f3b6b5ad8f4fb743d77b79c96c228b59c461d4
Harvey Bastidas <harveybc@ingeni-us.com>
2022-07-20 14:09:49 -0500

    Update dashboard.py

--- plugins/gui/visualizer_gui/blueprints/dashboard.py
+++ plugins/gui/visualizer_gui/blueprints/dashboard.py
@@ -74,6 +74,8 @@ def new_bp(plugin_folder, core_ep, store_ep, db):
     def min_training_mse():
         """ returns the fe_config.id for the minimum fe_training_error.mse for the current process"""
         # TODO:  use visualizer plugin function instead of store ep function if possible
+        db = get_db()
+        # TODO: create an automap model for the training_progress table
         process_list = current_app.config['FE'].ep_input.get_processes(current_user.id)
         return render_template("/process/index.html", process_list = process_list)
 
