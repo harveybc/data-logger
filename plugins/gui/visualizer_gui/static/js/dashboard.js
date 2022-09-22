5f7c84e3beeddca65f57aed14614f289ff2a1539
Harvey Bastidas <harveybc@ingeni-us.com>
2022-09-22 14:51:37 -0500

    Update dashboard.js

--- plugins/gui/visualizer_gui/static/js/dashboard.js
+++ plugins/gui/visualizer_gui/static/js/dashboard.js
@@ -11,8 +11,8 @@ export default {
     methods: {
         // returns an axios instance for basic authentication
         axiosBasicAuth(username, password) {
-          const buffer_auth = buffer.Buffer.from(username + ':' + password);
-          const b64 = buffer_auth.toString('base64');
+          let buffer_auth = buffer.Buffer.from(username + ':' + password);
+          let b64 = buffer_auth.toString('base64');
           return axios.create({
             headers: {
                 'Content-Type': 'application/json',
@@ -23,13 +23,13 @@ export default {
         // returns an axions instance with configured basic authentication
         // TODO: change to use current user
         axios_auth_instance(){
-          const axios_instance = this.axiosBasicAuth("test", "pass");
+          let axios_instance = this.axiosBasicAuth("test", "pass");
           return axios_instance; 
         },
         // call request that returns the config id for the best mse from table fe_training_error that has config.active == true
         best_online() {
           // setup authentication
-          axios_instance = this.axios_auth_instance();
+          let axios_instance = this.axios_auth_instance();
           // use the result of api request
           axios_instance.get('/best_online')
           .then((response) => {
@@ -41,7 +41,7 @@ export default {
         },
         // call request that returns the best mse from table fe_training_error that has config.active == true
         min_training_mse(){
-          axios_instance = this.axios_auth_instance();
+          let axios_instance = this.axios_auth_instance();
             // use the response of api request
             axios_instance.get('/min_training_mse')
             .then((response) => {
@@ -54,7 +54,7 @@ export default {
         // call request that returns the config id for the best mse from table fe_validation_error that has config.active == false
         best_config() {
           // setup authentication
-          axios_instance = this.axios_auth_instance();
+          let axios_instance = this.axios_auth_instance();
           // use the result of api request
           axios_instance.get('/best_config')
           .then((response) => {
@@ -66,7 +66,7 @@ export default {
         },
         // call request that returns the best mse from table fe_validation_error that has config.active == false
         min_validation_mse(){
-          axios_instance = this.axios_auth_instance();
+          let axios_instance = this.axios_auth_instance();
           // use the response of api request
           axios_instance.get('/min_validation_mse')
           .then((response) => {
