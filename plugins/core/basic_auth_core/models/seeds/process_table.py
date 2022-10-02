0db7015ec4c489c7818053671c0b8028253b25a5
Harvey Bastidas <harveybc@ingeni-us.com>
2022-10-02 13:04:31 -0500

    Update process_table.py

--- plugins/core/basic_auth_core/models/seeds/process_table.py
+++ plugins/core/basic_auth_core/models/seeds/process_table.py
@@ -31,7 +31,15 @@ def seed(app, db, table_name):
                 db.session.add(tmp0)
                 db.session.commit()
                 _logger.info("fe_config table seeded")
-
+            elif table_name == "fe_validation_error":
+                tmp0 =  table_base(id=0, mse=0.540289623, mae=-0.909284882, r2=0.28373445, config_id=0)
+                tmp1 =  table_base(id=1, mse=0.453582689, mae=-0.808478663, r2=0.708722942, config_id=0)
+                tmp2 =  table_base(id=2, mse=0.540289623, mae=-0.675440952, r2=0.960191341, config_id=0)
+                db.session.add(tmp0)
+                db.session.add(tmp1)
+                db.session.add(tmp2)
+                db.session.commit()
+                _logger.info("fe_validation_error table seeded") 
         except SQLAlchemyError as e:
             error = str(e)
             print("Error : " , error)
