240cf1882afcc379435101591fc1919c5c51d126
Harvey Bastidas <harveybc@ingeni-us.com>
2022-10-02 12:09:15 -0500

    Update dashboard.py

--- plugins/gui/visualizer_gui/blueprints/dashboard.py
+++ plugins/gui/visualizer_gui/blueprints/dashboard.py
@@ -81,6 +81,23 @@ def new_bp(plugin_folder, core_ep, store_ep, db):
             res = { 'error_ca' : error}
         attr = getattr(res, "config_id")
         return str(attr)
+
+    @bp.route("/best_config")
+    @login_required
+    def min_training_mse():
+        """ Returns the config id for the best mse from table fe_training_error that has config.active == true. """
+        # table base class
+        Base.prepare(db.engine, reflect=False)
+        # perform query, the column classs names are configured in config_store.json
+        try:
+            # res = db.session.query(func.min(Base.classes.fe_training_error.mse)).filter_by('some name', id = 5) 
+            res = db.session.query(Base.classes.fe_validation_error).join(Base.classes.fe_config, Base.classes.fe_validation_error.config_id == Base.classes.fe_config.id).filter(Base.classes.fe_config.active == False).order_by(asc(Base.classes.fe_training_error.mse)).first()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_ca' : error}
+        attr = getattr(res, "config_id")
+        return str(attr)
            
     def get_xy_training(pid):
         """ Returns the points to plot from the training_progress table. """
