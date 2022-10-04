1a6aa9c502be566b1f0389aa1617b770e214357a
Harvey Bastidas <harveybc@ingeni-us.com>
2022-10-04 08:31:54 -0500

    test

--- plugins/core/basic_auth_core/models/process_table.py
+++ plugins/core/basic_auth_core/models/process_table.py
@@ -41,14 +41,14 @@ class ProcessTable():
             if "index" not in c: c["index"] = False
             if "default" not in c: c["default"] = {}
             if "nullable" not in c: c["nullable"] = True
-
             if "primary_key" in c:
                 if c["primary_key"]:
                     t_args.append(Column(c["name"], col_type, autoincrement=True, primary_key=c["primary_key"], nullable=False))
                     pk_found = True
             if "foreign_key" in c:
                 if c["foreign_key"] != "none":
-                    t_args.append(Column(c["name"], "Integer", ForeignKey(c["foreign_key"]), unique=c["unique"], index=c["index"], default=c["default"], nullable=c["nullable"]))
+                    t_args.append(Column(c["name"], "Integer", ForeignKey(c["foreign_key"]), unique=c["unique"], index=c["index"], default=c["default"], nullable=False))
+                    rel_found = True
             if "primary_key" not in c and "foreign_key" not in c:
                 t_args.append(Column(c["name"], col_type, primary_key=False, unique=c["unique"], index=c["index"], default=c["default"], nullable=c["nullable"]))
             if c["name"] == "timestamp":
