db3848fb90001eb4d1e98f7f8637cc6a659e4910
Harvey Bastidas <harveybc@ingeni-us.com>
2022-10-05 13:22:50 -0500

    Update __init__.py

--- plugins/core/basic_auth_core/__init__.py
+++ plugins/core/basic_auth_core/__init__.py
@@ -190,33 +190,4 @@ class BasicAuthCore():
         _logger.info("Data structure created")
         self.seed_init_data(app, db)
         _logger.info("Initial data seed done")
-        
-    def column_max(self, db, table, column):
-        """ Returns the maximum of the selected field(column) belonging to the user_id from the specified table. """
-        # sanitize the input string and limit its length
-        table_name = sanitize_str(table, 256)
-        column_name = sanitize_str(column, 256)
-        Base.prepare(db.engine, reflect=False)
-        # table base class
-        table_base_column = eval("Base.classes." + table_name + "." + column_name )
-        # perform query
-        try:
-            res = db.session.query(func.max(table_base_column)) 
-        except SQLAlchemyError as e:
-            error = str(e)
-            print("Error : " , error)
-            res = { 'error_ca' : error}
-        print(str(res))
-        return json.dumps(str(res.first()))
-
-    def get_count(self, table):
-        """Returns the count of rows in the specified table. """
-        db = get_db()
-        #user_id = self.get_user_id(username)
-        row = db.execute(
-            "SELECT COUNT(id) FROM " + table
-        ).fetchone() 
-        result = dict(row)        
-        return result
-            
-        
+                
