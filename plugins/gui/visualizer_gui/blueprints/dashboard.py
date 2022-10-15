d3002cd2af2334b9deded9e5d681110070b2e8d7
Harvey Bastidas <harveybc@ingeni-us.com>
2022-10-15 15:18:28 -0500

    Update dashboard.py

--- plugins/gui/visualizer_gui/blueprints/dashboard.py
+++ plugins/gui/visualizer_gui/blueprints/dashboard.py
@@ -19,7 +19,7 @@ from app.app import load_plugin_config
 from app.util import as_dict
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy import func, asc
+from sqlalchemy import func, asc, desc
 import json
 
 
@@ -139,11 +139,15 @@ def new_bp(plugin_folder, core_ep, store_ep, db, Base):
         # perform query, the column classs names are configured in config_store.json
         try:
             res = db.session.query(Base.classes.fe_training_error).join(Base.classes.fe_config, Base.classes.fe_training_error.config_id == Base.classes.fe_config.id).filter(Base.classes.fe_config.active == True).order_by(asc(Base.classes.fe_training_error.mse)).first_or_404()
+            best_online_config = getattr(res, "config_id")
+            res = db.session.query(Base.classes.fe_training_error).filter(Base.classes.fe_training_error.config_id == best_online_config).order_by(desc(Base.classes.fe_training_error.timestamp)).limit(10).all()
+            
         except SQLAlchemyError as e:
             error = str(e)
             print("Error : " , error)
             res = { 'error_ca' : error}
-        attr = getattr(res, "mse")
+
+        
         return str(attr)
     
     
