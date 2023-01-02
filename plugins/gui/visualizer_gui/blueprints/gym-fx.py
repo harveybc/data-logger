a729164e3e5cbff2a7e41df56ff1b26bf0d3316d
Ubuntu <ubuntu@ip-172-31-81-111.ec2.internal>
2023-01-02 15:34:22 +0000

    autopep8 on gym-fx blueprint

--- plugins/gui/visualizer_gui/blueprints/gym-fx.py
+++ plugins/gui/visualizer_gui/blueprints/gym-fx.py
@@ -1,6 +1,6 @@
 
 # This file contains the data_logger plugin, th input plugin can load all the data or starting from
- # the last id.
+# the last id.
 
 from flask import Blueprint
 from flask import flash
@@ -26,34 +26,36 @@ import json
 def new_bp(plugin_folder, core_ep, store_ep, db, Base):
 
     # construct the data_logger blueprint using the plugin folder as template folder
-    bp = Blueprint("gym_fx_bp", __name__, template_folder=plugin_folder+"/templates")
-    
+    bp = Blueprint("gym_fx_bp", __name__,
+                   template_folder=plugin_folder+"/templates")
+
     # returns the config id for the best mse from table fe_training_error that has config.active == true
     @bp.route("/gym-fx")
     @login_required
     def gym_fx():
         """ Returns the config id for the best mse from table fe_training_error that has config.active == true. """
         if request.method == 'POST':
-        	
-        	#self.data_log(validation_score=v_score, avg_score_v=avg_score_v, training_score=score, avg_score=avg_score, info=info)
-	        validation_score = request.form['validation_scoreme']
-			avg_score_v = request.form['avg_score_v']
-			training_score = request.form[' training_score']
-			avg_score = request.form['avg_score']
-			info = request.form['info' ]
-	        db = get_db()
-	        error = None
+
+            # self.data_log(validation_score=v_score, avg_score_v=avg_score_v, training_score=score, avg_score=avg_score, info=info)
+            validation_score = request.form['validation_scoreme']
+            avg_score_v = request.form['avg_score_v']
+            training_score = request.form[' training_score']
+            avg_score = request.form['avg_score']
+            info = request.form['info']
+            db = get_db()
+            error = None
         # table base class
-        #Base.prepare(db.engine) 
+        # Base.prepare(db.engine)
         # perform query, the column classs names are configured in config_store.json
         try:
-            # res = db.session.query(func.min(Base.classes.fe_training_error.mse)).filter_by('some name', id = 5) 
-            res = db.session.query(Base.classes.fe_training_error).join(Base.classes.fe_config, Base.classes.fe_training_error.config_id == Base.classes.fe_config.id).filter(Base.classes.fe_config.active == True).order_by(asc(Base.classes.fe_training_error.mse)).first_or_404()
+            # res = db.session.query(func.min(Base.classes.fe_training_error.mse)).filter_by('some name', id = 5)
+            res = db.session.query(Base.classes.fe_training_error).join(Base.classes.fe_config, Base.classes.fe_training_error.config_id == Base.classes.fe_config.id).filter(
+                Base.classes.fe_config.active == True).order_by(asc(Base.classes.fe_training_error.mse)).first_or_404()
         except SQLAlchemyError as e:
             error = str(e)
-            print("Error : " , error)
-            res = { 'error_ca' : error}
+            print("Error : ", error)
+            res = {'error_ca': error}
         attr = getattr(res, "config_id")
-        
+
         return str(attr)
     return bp
