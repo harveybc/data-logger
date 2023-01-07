0a99da70b79ad56616c042c51d24a3680afe80d4
harveybv <harveybc@ingeni-us.com>
2023-01-07 14:13:00 -0500

    update

--- plugins/gui/visualizer_gui/blueprints/gym-fx.py
+++ plugins/gui/visualizer_gui/blueprints/gym-fx.py
@@ -34,29 +34,18 @@ def new_bp(plugin_folder, core_ep, store_ep, db, Base):
             """ Creates a new register using Automap Base. """
             # create new register
             try:
-                with app.app_context():
-                    gym_fx_class = Base.classes.gym_fx
-                    new_reg = gym_fx_class()
-                    new_reg.score = request.form['score_v']
-                    new_reg.avg_score_v = request.form['avg_score_v']
-                    new_reg.score_v = request.form[' training_score']
-                    new_reg.avg_score = request.form['avg_score']
-                    info = json.loads(request.form['info' ])
-                    new_reg.reward = info['reward']
-                    new_reg.balance = info['balance']
-                    new_reg.equity = info['equity']
-                    new_reg.margin = info['margin']
-                    new_reg.config_id = config_id
-                    db = get_db()
-                    error = None
-                    db.session.add(new_reg)
-                gym_fx_class = Base.classes.gym_fx_data
+                gym_fx_class = Base.classes.gym_fx
                 new_reg = gym_fx_class()
-                new_reg.validation_score = request.form['validation_score']
+                new_reg.score = request.form['score_v']
                 new_reg.avg_score_v = request.form['avg_score_v']
-                new_reg.training_score = request.form[' training_score']
+                new_reg.score_v = request.form[' training_score']
                 new_reg.avg_score = request.form['avg_score']
-                info = request.form['info' ]
+                info = json.loads(request.form['info' ])
+                new_reg.reward = info['reward']
+                new_reg.balance = info['balance']
+                new_reg.equity = info['equity']
+                new_reg.margin = info['margin']
+                new_reg.config_id = config_id
                 db = get_db()
                 error = None
                 db.session.add(new_reg)
