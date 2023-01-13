83a61d67c695693200e14e6c0c5c298288de47f5
Harvey Bastidas <harveybc@ingeni-us.com>
2023-01-13 18:47:46 -0500

    Update dashboard.js

--- plugins/gui/visualizer_gui/static/js/dashboard.js
+++ plugins/gui/visualizer_gui/static/js/dashboard.js
@@ -20,7 +20,7 @@ export default {
         //this.get_process_list();
         //this.get_process();
         //this.get_status();
-        this.best_online_ = this.best_online();
+        this.best_online_ = this.gymfx_best_online_();
         this.min_training_mse_ = this.min_training_mse();
         this.best_config_ = this.best_config();
         this.min_validation_mse_ = this.min_validation_mse();
