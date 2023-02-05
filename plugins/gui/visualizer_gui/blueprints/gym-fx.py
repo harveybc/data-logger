48dc89bb3fe6ac9d6d4bff9e82543d2641df6cf8
Harvey Bastidas <harveybc@ingeni-us.com>
2023-02-05 15:00:24 -0500

    splitted gym-fx from dashboard

--- plugins/gui/visualizer_gui/blueprints/gym-fx.py
+++ plugins/gui/visualizer_gui/blueprints/gym-fx.py
@@ -59,4 +59,93 @@ def new_bp(plugin_folder, core_ep, store_ep, db, Base):
                 new_reg = error
                 return new_reg
             return as_dict(new_reg)
+    
+    # returns the config id for the best mse from table gym_fx_data that has config.active == true
+    @bp.route('/gymfx_best_online_')
+    @login_required
+    def gymfx_best_online_():
+        """ Returns the config id for the best mse from table gym_fx_data that has config.active == true. """
+        # table base class
+        #Base.prepare(db.engine)
+        # perform query, the column classs names are configured in config_store.json
+        try:
+            # query for the maximum reward from the gym_fx_data table for the config_id whose gymfx_config.active == True
+            res = db.session.query(Base.classes.gym_fx_data).join(Base.classes.gym_fx_config, Base.classes.gym_fx_data.config_id == Base.classes.gym_fx_config.id).filter(Base.classes.gym_fx_config.active == True).order_by(desc(Base.classes.gym_fx_data.score)).first_or_404()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("SQLAlchemyError : " , error)
+            return error
+        except Exception as e:
+            error = str(e)
+            print("Error : " , error)
+            return error
+        attr = getattr(res, "config_id")
+        return str(attr)
+    
+    @bp.route("/gymfx_max_training_score_")
+    @login_required
+    def gymfx_max_training_score_():
+        """ Returns the best mse from table gym_fx_data that has config.active == true. """
+        # table base class
+        #Base.prepare(db.engine)
+        # perform query, the column classs names are configured in config_store.json
+        try:
+            res = db.session.query(Base.classes.gym_fx_data).join(Base.classes.gym_fx_config, Base.classes.gym_fx_data.config_id == Base.classes.gym_fx_config.id).filter(Base.classes.gym_fx_config.active == True).order_by(desc(Base.classes.gym_fx_data.score)).first_or_404()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_ca' : error}
+        attr = getattr(res, "mse")
+        return str(attr)
+
+    @bp.route("/gymfx_best_config_")
+    @login_required
+    def gymfx_best_config_():
+        """ Returns the config id for the best mse from table gym_fx_data that has config.active == false. """
+        # table base class
+        #Base.prepare(db.engine)
+        # perform query, the column classs names are configured in config_store.json
+        try:
+            res = db.session.query(Base.classes.gym_fx_data).join(Base.classes.gym_fx_config, Base.classes.gym_fx_data.config_id == Base.classes.gym_fx_config.id).filter(Base.classes.gym_fx_config.active == False).order_by(desc(Base.classes.gym_fx_data.score_v)).first_or_404()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_ca' : error}
+        attr = getattr(res, "config_id")
+        return str(attr)
+           
+    @bp.route("/gymfx_max_validation_score_")
+    @login_required
+    def gymfx_max_validation_score_():
+        """ Returns the best mse from table gym_fx_data that has config.active == false. """
+        # table base class
+        #Base.prepare(db.engine)
+        # perform query, the column classs names are configured in config_store.json
+        try:
+            res = db.session.query(Base.classes.gym_fx_data).join(Base.classes.gym_fx_config, Base.classes.gym_fx_data.config_id == Base.classes.gym_fx_config.id).filter(Base.classes.gym_fx_config.active == False).order_by(desc(Base.classes.gym_fx_data.score_v)).first_or_404()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_ca' : error}
+        attr = getattr(res, "mse")
+        return str(attr)
+    
+    @bp.route("/online_mse_list")
+    @login_required
+    def online_mse_list():
+        """ Returns the best mse from table gym_fx_data that has config.active == true. """
+        # table base class
+        #Base.prepare(db.engine)
+        # perform query, the column classs names are configured in config_store.json
+        try:
+            res = db.session.query(Base.classes.gym_fx_data).join(Base.classes.gym_fx_config, Base.classes.gym_fx_data.config_id == Base.classes.gym_fx_config.id).filter(Base.classes.gym_fx_config.active == True).order_by(asc(Base.classes.gym_fx_data.mse)).first_or_404()
+            best_online_config = getattr(res, "config_id")
+            res = db.session.query(Base.classes.gym_fx_data).filter(Base.classes.gym_fx_data.config_id == best_online_config).order_by(desc(Base.classes.gym_fx_data.timestamp)).limit(10).all()
+            # convert to list of lists of timestamp and mse
+            res = [[row.timestamp, row.mse] for row in res]
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_ca' : error}        
+        return json.dumps(res)
     return bp
