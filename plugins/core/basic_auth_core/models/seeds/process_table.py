3ddc85d8516436391462e312a1acbcaed5ffe67b
Harvey Bastidas <harveybc@ingeni-us.com>
2023-02-05 20:56:40 -0500

    Update process_table.py

--- plugins/core/basic_auth_core/models/seeds/process_table.py
+++ plugins/core/basic_auth_core/models/seeds/process_table.py
@@ -50,8 +50,8 @@ def seed(app, db, table_name):
                 db.session.commit()
                 _logger.info("gym_fx_config table seeded") 
             elif table_name == "gym_fx_data":
-                tmp0 =  table_base(score=10000, score_v=9000, config_id=-1)
-                tmp1 =  table_base(score=1000, score_v=900, config_id=-2)
+                tmp0 =  table_base(score=10000, score_v=9000, config_id=1)
+                tmp1 =  table_base(score=1000, score_v=900, config_id=2)
                 db.session.add(tmp0)
                 db.session.add(tmp1)
                 db.session.commit()
