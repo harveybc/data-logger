96a7c297b712d0f3ece497ec405f7688933ca4d1
Harvey Bastidas <harveybc@ingeni-us.com>
2023-02-22 04:21:38 -0500

    Update gym-fx.py

--- plugins/gui/visualizer_gui/blueprints/gym-fx.py
+++ plugins/gui/visualizer_gui/blueprints/gym-fx.py
@@ -156,24 +156,19 @@ def new_bp(plugin_folder, core_ep, store_ep, db, Base):
     @bp.route("/gymfx_validation_plot")
     @login_required
     def gymfx_validation_plot_():
-        """ Returns a json with arrays for the columns balance,equity,margin,reward from the gym_fx_data table for thebest prcess with config_id.active== True. """
+        """ Returns a json with the initial capital and arrays for the columns order_status, tick_date, balance, equity,margin,reward from the gym_fx_data table for thebest prcess with config_id.active== True. """
         args = request.args
-        num_points = args.get("num_points", default=1920, type=int)
-        
+        num_points = args.get("num_points", default=1000, type=int)
         # perform query, the column classs names are configured in config_store.json
         try:
-            best = int(gymfx_best_online_())
-            print("best : " , best)
+            best = int(gymfx_best_offline_())
+            print("best_offline : " , best)
             points = db.session.query(Base.classes.gym_fx_data).filter(Base.classes.gym_fx_data.config_id == best ).order_by(desc(Base.classes.gym_fx_data.id)).limit(num_points).all()
-            res = []
-            count = 0
-            for p in points:
-                res.append({"x":count, "y":p.score})
-                count += 1
+            
         except Exception as e:
             error = str(e)
             print("Error : " , error)
             return error
-        return json.dumps(res)
+        return json.dumps(as_dict(points))
 
     return bp
