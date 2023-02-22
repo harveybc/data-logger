5c2a363788ebcc132e3e8c366cbfb39010196edb
Harvey Bastidas <harveybc@ingeni-us.com>
2023-02-22 15:03:07 -0500

    Update process_table.py

--- plugins/core/basic_auth_core/models/seeds/process_table.py
+++ plugins/core/basic_auth_core/models/seeds/process_table.py
@@ -17,31 +17,15 @@ def seed(app, db, table_name):
         table_base = eval("Base.classes." + table_name)
         # perform query
         try:
-            if table_name == "fe_training_error":
-                tmp0 =  table_base(mse=0.540289623, mae=-0.909284882, r2=0.28373445, config_id=1)
-                tmp1 =  table_base(mse=0.453582689, mae=-0.808478663, r2=0.708722942, config_id=1)
-                tmp2 =  table_base(mse=0.540289623, mae=-0.675440952, r2=0.960191341, config_id=1)
+            if table_name == "gym_fx_validation_plot":
+                tmp0 =  table_base(balance=10000, equity=10000, order_status=0 ,config_id=2, tick_timestamp=0)
+                tmp1 =  table_base(balance=10000, equity=13000, order_status=1 ,config_id=2, tick_timestamp=1)
+                tmp2 =  table_base(balance=10000, equity=7000, order_status=-1 ,config_id=2, tick_timestamp=2)
                 db.session.add(tmp0)
                 db.session.add(tmp1)
                 db.session.add(tmp2)
                 db.session.commit()
-                _logger.info("fe_training_error table seeded")        
-            elif table_name == "fe_config":
-                tmp0 =  table_base(active=True)
-                tmp1 =  table_base(active=False)
-                db.session.add(tmp0)
-                db.session.add(tmp1)
-                db.session.commit()
-                _logger.info("fe_config table seeded")
-            elif table_name == "fe_validation_error":
-                tmp0 =  table_base(mse=0.540289623, mae=-0.909284882, r2=0.28373445, config_id=2)
-                tmp1 =  table_base(mse=0.453582689, mae=-0.808478663, r2=0.708722942, config_id=2)
-                tmp2 =  table_base(mse=0.540289623, mae=-0.675440952, r2=0.960191341, config_id=2)
-                db.session.add(tmp0)
-                db.session.add(tmp1)
-                db.session.add(tmp2)
-                db.session.commit()
-                _logger.info("fe_validation_error table seeded") 
+                _logger.info("gym_fx_validation_plot table seeded") 
             elif table_name == "gym_fx_config":
                 tmp0 =  table_base(initial_capital=10000, active=True)
                 tmp1 =  table_base(initial_capital=1000, active=False)
