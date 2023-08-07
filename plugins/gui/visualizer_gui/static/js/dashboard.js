1afd95de7b7622748ae5cfefce08adc475db1490
Harvey Bastidas <harveybc@ingeni-us.com>
2023-08-07 18:14:07 -0500

    update

--- plugins/gui/visualizer_gui/static/js/dashboard.js
+++ plugins/gui/visualizer_gui/static/js/dashboard.js
@@ -147,7 +147,7 @@ export class Dashboard {
       that.data_ = that.transform_validation_plot_data(plot_data);
       console.log("that.data_.xy_equity = " + that.data_.xy_equity);
       // TODO: update validation_plot_data and options.grid.markings function
-      // for each that.data_ append to val_list tbody element
+      // for each that.data_ a ppend to val_list tbody element
       that.val_list_update();      
 
 
