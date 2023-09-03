ce05c5904be37ba6d89d760d6ff9b88ab58b435c
Harvey Bastidas <harveybc@ingeni-us.com>
2023-09-03 15:48:05 -0500

    test

--- plugins/gui/visualizer_gui/blueprints/process.py
+++ plugins/gui/visualizer_gui/blueprints/process.py
@@ -15,6 +15,7 @@ from flask_login import current_user
 from app.db import get_db
 from flask import current_app
 from flask import jsonify
+from app.app import load_plugin_config
 
 
 def new_bp(plugin_folder, core_ep, store_ep, db, Base):
