bacd1729cef1f5e85c6fd108a34dc8c3063028a6
Harvey Bastidas <harveybc@ingeni-us.com>
2023-09-21 21:47:07 -0500

    Update users.js

--- plugins/gui/visualizer_gui/static/js/users.js
+++ plugins/gui/visualizer_gui/static/js/users.js
@@ -64,7 +64,7 @@ export class Users {
   }
 
   users_list_update(start, data_) {
-    var users_list = "";s
+    var users_list = "";
     for (let i = start; i < data_.length; i++) {
       users_list += (`
       <tr>
