3d2dff5afa1769edb2b6e75f8fe95848fb4ab8af
Harvey Bastidas <harveybc@ingeni-us.com>
2023-12-19 13:23:55 -0500

    Update dashboard.py

--- plugins/gui/visualizer_gui/blueprints/dashboard.py
+++ plugins/gui/visualizer_gui/blueprints/dashboard.py
@@ -100,7 +100,7 @@ def new_bp(plugin_folder, core_ep, store_ep, db, Base):
             current_user.id)
         return render_template("/process/index.html", process_list=process_list)
 
-    @bp.route("/logs")
+    @bp.route("/dashboard/logs")
     @login_required
     def log_index():
         """Show the logs index."""
