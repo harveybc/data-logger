5d5c858f4293988ffc4a86d34cbbe5cb9d737a04
Harvey Bastidas <harveybc@ingeni-us.com>
2024-02-05 01:40:48 -0500

    update

--- plugins/core/basic_auth_core/__init__.py
+++ plugins/core/basic_auth_core/__init__.py
@@ -10,7 +10,7 @@ from .models.authorization import Authorization
 from .models.log import Log
 from .models.process import Process
 from .models.process_table import ProcessTable
-from .models.configs import Configs
+
 
 #from .models.process_register_factory import ProcessRegisterFactory
 from sqlalchemy.ext.automap import automap_base
@@ -20,8 +20,9 @@ from copy import deepcopy
 from app.util import sanitize_str
 from sqlalchemy import func
 import warnings
+# import controllers
+from .controllers.configs import ConfigsController
 
-Base = automap_base()
 
 import os
 
@@ -46,7 +47,7 @@ class BasicAuthCore():
         self.Log = Log 
         self.Process = Process
         self.ProcessTable = ProcessTable
-        self.Configs = Configs
+        self.ConfigsController = ConfigsController
         # seed initial user 
 
     def seed_init_data(self, app, db):
@@ -98,7 +99,7 @@ class BasicAuthCore():
             else:
                 return -1
 
-    def create_table(self, app, db, table):
+    def create_table(self, app, db, table, Base):
         """ Create a table if another with the same name does not exist.
             Args:
             app (Flask): the current flask app instance.
--- plugins/core/basic_auth_core/controllers/configs.py
+++ plugins/core/basic_auth_core/controllers/configs.py
@@ -31,148 +31,150 @@ from functools import wraps
 from flask import (current_app)
 from flask import request
 
-#@authorization_required
-#@log_required
-def create(body, Base):
-    """ Create a register in db based on a json from a request's body parameter.
+class ConfigsController():
+            
+    #@authorization_required
+    #@log_required
+    def create(body, Base):
+        """ Create a register in db based on a json from a request's body parameter.
 
-        Args:
-        body (dict): dict containing the fields of the new register, obtained from json in the body of the request.
+            Args:
+            body (dict): dict containing the fields of the new register, obtained from json in the body of the request.
 
-        Returns:
-        res (dict): the newly created register.
-    """
-    # instantiate user with the body dict as kwargs
-    ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
-    new = ConfigsModel(**body)
-    # create new flask-sqlalchemy session
-    db.session.add(new)
-    try:
-        db.session.commit()
-        new_id =  new.id
-        #db.session.expunge_all()
-        #db.session.close()
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        print("Error: ", error)
-        return error
-    # test if the new user was created 
-    try:
-        res = ConfigsModel.query.filter_by(id=new_id).one()
-    except SQLAlchemyError as e:
-        error = str(e.__dict__['orig'])
-        print("Error: ", error)
-        return error
-    # return register as dict
-    return as_dict(res)
+            Returns:
+            res (dict): the newly created register.
+        """
+        # instantiate user with the body dict as kwargs
+        ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
+        new = ConfigsModel(**body)
+        # create new flask-sqlalchemy session
+        db.session.add(new)
+        try:
+            db.session.commit()
+            new_id =  new.id
+            #db.session.expunge_all()
+            #db.session.close()
+        except SQLAlchemyError as e:
+            error = str(e.__dict__['orig'])
+            print("Error: ", error)
+            return error
+        # test if the new user was created 
+        try:
+            res = ConfigsModel.query.filter_by(id=new_id).one()
+        except SQLAlchemyError as e:
+            error = str(e.__dict__['orig'])
+            print("Error: ", error)
+            return error
+        # return register as dict
+        return as_dict(res)
 
-#@authorization_required
-def read(authorization_id, Base):
-    """ Performs a query log register.
+    #@authorization_required
+    def read(authorization_id, Base):
+        """ Performs a query log register.
 
-        Args:
-        process_id (str): authorization_id (log/<authorization_id>).
+            Args:
+            process_id (str): authorization_id (log/<authorization_id>).
 
-        Returns:
-        res (dict): the requested  register.
-    """
-    ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
-    try:
-        res = as_dict(ConfigsModel.read(id=authorization_id))
-    except SQLAlchemyError as e:
-        error = str(e)
-        print("Error : " , error)
-        return error 
-    return res
+            Returns:
+            res (dict): the requested  register.
+        """
+        ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
+        try:
+            res = as_dict(ConfigsModel.read(id=authorization_id))
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            return error 
+        return res
 
-#@authorization_required
-#@log_required
-def update(authorization_id, body, Base):
-    """ Update a register in db based on a json from a request's body parameter.
+    #@authorization_required
+    #@log_required
+    def update(authorization_id, body, Base):
+        """ Update a register in db based on a json from a request's body parameter.
 
-        Args:
-        user_id (str): id field of the model, obtained from url parameter (log/<authorization_id>).
-        body (dict): dict containing the fields of the register, obtained from json in the body of the request.
+            Args:
+            user_id (str): id field of the model, obtained from url parameter (log/<authorization_id>).
+            body (dict): dict containing the fields of the register, obtained from json in the body of the request.
 
-        Returns:
-        res (dict): the updated register
-    """
-     # query the existing register
-    ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
-    try:
-        process_model = ConfigsModel.read(id=authorization_id)
-        for property, value in body.items():
-            setattr(process_model, property, value)
-    except SQLAlchemyError as e:
-        error = str(e)
-        print("Error : " , error)
-        res = { 'error_a' : error}
-    # replace model with body fields
-    
-    # perform update 
-    try:
-        db.session.commit()
-        #db.session.expunge_all()
-        #db.session.close()
-    except SQLAlchemyError as e:
-        error = str(e)
-        print("Error : " , error)
-        res = { 'error_b' : error}
-    # test if the model was updated 
-    try:
-        res = as_dict(ConfigsModel.read(id=int(authorization_id)))
-        #db.session.expunge_all()
-        #db.session.close()
-    except SQLAlchemyError as e:
-        error = str(e)
-        print("Error : " , error)
-        res = { 'error_c' : error}
-    return res
+            Returns:
+            res (dict): the updated register
+        """
+        # query the existing register
+        ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
+        try:
+            process_model = ConfigsModel.read(id=authorization_id)
+            for property, value in body.items():
+                setattr(process_model, property, value)
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_a' : error}
+        # replace model with body fields
+        
+        # perform update 
+        try:
+            db.session.commit()
+            #db.session.expunge_all()
+            #db.session.close()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_b' : error}
+        # test if the model was updated 
+        try:
+            res = as_dict(ConfigsModel.read(id=int(authorization_id)))
+            #db.session.expunge_all()
+            #db.session.close()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            res = { 'error_c' : error}
+        return res
 
-#@authorization_required
-#@log_required
-def delete(authorization_id, Base):
-    """ Delete a register in db based on the id field of the authorizarions model, obtained from a request's authorization_id url parameter.
+    #@authorization_required
+    #@log_required
+    def delete(authorization_id, Base):
+        """ Delete a register in db based on the id field of the authorizarions model, obtained from a request's authorization_id url parameter.
 
-        Args:
-        process_id (str): id field , obtained from a request's url parameter (log/<authorization_id>).
+            Args:
+            process_id (str): id field , obtained from a request's url parameter (log/<authorization_id>).
 
-        Returns:
-        res (int): the deleted register id field
-    """
-    ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
-    try:
-        res = ConfigsModel.getmodel(id=authorization_id).one()
-    except SQLAlchemyError as e:
-        error = str(e)
-        print("Error : " , error)
-        return error
-    # perform delete 
-    db.session.delete(res)
-    try:
-        db.session.commit()
-    except SQLAlchemyError as e:
-        error = str(e)
-        print("Error : " , error)
-        return error
-    return { "id" : res.id }
+            Returns:
+            res (int): the deleted register id field
+        """
+        ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
+        try:
+            res = ConfigsModel.getmodel(id=authorization_id).one()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            return error
+        # perform delete 
+        db.session.delete(res)
+        try:
+            db.session.commit()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            return error
+        return { "id" : res.id }
 
-#@authorization_required
-def read_all(Base):
-    """ Query all registers of the logs table.
+    #@authorization_required
+    def read_all(Base):
+        """ Query all registers of the logs table.
 
-        Returns:
-        res (dict): the requested list.
-    """ 
-    ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
-    try:
-        res = ConfigsModel.read_all()
-    except SQLAlchemyError as e:
-        error = str(e)
-        print("Error : " , error)
-        return error
-    # convert to list of dicts and empty pass
-    res2 =[]
-    for r in res:
-        res2.append(as_dict(r))
-    return res2
+            Returns:
+            res (dict): the requested list.
+        """ 
+        ConfigsModel = ProcessRegisterFactory("gym_fx_config", Base)
+        try:
+            res = ConfigsModel.read_all()
+        except SQLAlchemyError as e:
+            error = str(e)
+            print("Error : " , error)
+            return error
+        # convert to list of dicts and empty pass
+        res2 =[]
+        for r in res:
+            res2.append(as_dict(r))
+        return res2
