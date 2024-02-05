9051d2121377e87046b41795f3ab152a12c2e807
Harvey Bastidas <harveybc@ingeni-us.com>
2024-02-05 03:17:49 -0500

    Update configs.js

--- plugins/gui/visualizer_gui/static/js/configs.js
+++ plugins/gui/visualizer_gui/static/js/configs.js
@@ -88,6 +88,7 @@ export class Configs {
         <td>${data_[i].initial_capital}</td>
         <td>${data_[i].paused}</td>
         <td>${data_[i].status}</td>
+        <td><a href="/configs/update/${data_[i].id}"><i class="fas fa-pen"></i></a></td>
       </tr>
       `);
     }
