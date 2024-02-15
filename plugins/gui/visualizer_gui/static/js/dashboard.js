41b65a18acfe05ab71f936aa6407b343b762da38
Harvey Bastidas <harveybc@ingeni-us.com>
2024-02-14 20:35:55 -0500

    Update dashboard.js

--- plugins/gui/visualizer_gui/static/js/dashboard.js
+++ plugins/gui/visualizer_gui/static/js/dashboard.js
@@ -554,7 +554,7 @@ export class Dashboard {
 
   // read values from the server
   update_scoreboard() {
-    that=this;
+    var that=this;
     this.gymfx_best_online_().then((response) => {
       that.gymfx_best_online = response.data;
       document.getElementById('box_0_value').innerHTML = that.gymfx_best_online;
