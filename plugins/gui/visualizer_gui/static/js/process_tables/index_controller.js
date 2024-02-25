05876858f732b4f76bdb80cb16c7c44c1692eb4a
Harvey Bastidas <harveybc@ingeni-us.com>
2024-02-25 14:09:33 -0500

    Update index_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
@@ -10,8 +10,9 @@ export class IndexController {
     // get gymfx_process_list data from the server
     var that = this;
     this.request_view_data().then((response) => {
-      that.index_list_update(0, 20, response.data.json());
-      console.log("response.data:" + response.data.json());
+      res = JSON.parse(response.data);
+      that.index_list_update(0, 20, res);
+      console.log("response.data:" +JSON.stringify(res));
     })
   }
 
