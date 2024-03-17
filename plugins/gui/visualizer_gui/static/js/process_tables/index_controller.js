160f334997e412f65a59f0b7e24f5ab180f1369d
Harvey Bastidas <harveybc@ingeni-us.com>
2024-03-17 18:15:19 -0500

    Update index_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
@@ -42,7 +42,7 @@ export class IndexController {
         } else {
           this.realtime = 'off'
         }
-      }, bind(this));
+      }).bind(this);
       /*
         * END INTERACTIVE CHART
       */
