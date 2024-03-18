20aba797184bf815610fd8d07520cb9420bff80d
Harvey Bastidas <harveybc@ingeni-us.com>
2024-03-17 21:30:11 -0500

    Update index_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
@@ -280,7 +280,7 @@ export class IndexController {
     this.gymfx_online_plot_().then((response) => {
       //console.log("pre:" + JSON.stringify(response.data));
       that.xy_points_ = that.transform_plot_data(response.data);
-      //console.log("update:" + JSON.stringify(this.xy_points_));
+      console.log("update:" + JSON.stringify(this.xy_points_));
       try {
         //this.interactive_plot.setData(this.xy_points_);
         that.interactive_plot.setData([that.xy_points_]);
