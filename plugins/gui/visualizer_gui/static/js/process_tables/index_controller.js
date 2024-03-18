db23b2e21ed4697be213d7ff04768921e04f6043
Harvey Bastidas <harveybc@ingeni-us.com>
2024-03-17 20:49:43 -0500

    Update index_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
@@ -30,22 +30,18 @@ export class IndexController {
     this.gymfx_online_plot_().then((response) => {
       //console.log("pre:" + JSON.stringify(response.data));
       that.xy_points_ = that.transform_plot_data(response.data);
-      //console.log("update:" + JSON.stringify(this.xy_points_));
-      try {
-        //this.interactive_plot.setData(this.xy_points_);
-        that.interactive_plot.setData([that.xy_points_]);
-        //Since the axes don't change, we don't need to call plot.setupGrid()
-        that.interactive_plot.draw();
-      } catch (e) {
-        console.log(e);
-      }
+      console.log("update:" + JSON.stringify(this.xy_points_));
       //if (that.realtime === 'on')
       //  setTimeout(function () { this.rt_update(); }.bind(that), 1000);
     }, (error) => {
       console.log(error);
     });
+    //this.interactive_plot.setData(this.xy_points_);
+    that.interactive_plot.setData([that.xy_points_]);
+    //Since the axes don't change, we don't need to call plot.setupGrid()
+    that.interactive_plot.draw();
     // Draw interactive plot
-    this.interactive_plot = $.plot('#interactive', this.xy_points_ , {
+    this.interactive_plot = $.plot('#interactive', [this.xy_points_] , {
       grid: {
         borderColor: '#f3f3f3',
         borderWidth: 1,
