8721b3c3ec6ad280486459b18979d054fcd4cc86
Harvey Bastidas <harveybc@ingeni-us.com>
2024-03-17 21:59:49 -0500

    Update index_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
@@ -26,7 +26,7 @@ export class IndexController {
       this.scoreboard_update();
     })
    // Online Plot
-    that.interactive_plot = $.plot('#interactive', [], {
+    this.interactive_plot = $.plot('#interactive', [], {
       grid: {
         borderColor: '#f3f3f3',
         borderWidth: 1,
