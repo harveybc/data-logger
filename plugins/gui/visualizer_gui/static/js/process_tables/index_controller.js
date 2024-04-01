cc812bbedcfe4465c363e4ab93bb7c4f85d05240
Harvey Bastidas <harveybc@ingeni-us.com>
2024-04-01 02:02:38 -0500

    update

--- plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/index_controller.js
@@ -144,9 +144,9 @@ export class IndexController {
               p_list += (`<td>${data_[i][col]}</td>`);
             }
             //p_list += "<td>View</td><td>Edit</td><td>Delete</td>"
-            p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_detail?id=" + data_[i]["id"] +  '"><i class="fas fa-eye"></i></a></td>';
-            p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_edit?id=" + data_[i]["id"] + '"><i class="fas fa-pen"></i></a></td>';
-            p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_remove?id=" + data_[i]["id"] + '"><i class="fas fa-trash"></i></a></td>';
+            p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_detail/" + data_[i]["id"] +  '"><i class="fas fa-eye"></i></a></td>';
+            p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_edit/" + data_[i]["id"] + '"><i class="fas fa-pen"></i></a></td>';
+            p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_remove/" + data_[i]["id"] + '"><i class="fas fa-trash"></i></a></td>';
             p_list += (`</tr>`);
           }
       // else show all columns in the table.columns list
@@ -158,9 +158,9 @@ export class IndexController {
           let col = table.columns[j].name;
           p_list += (`<td>${data_[i][col]}</td>`);
         }
-        p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_detail?id=" + data_[i]["id"] + '"><i class="fas fa-eye"></i></a></td>';
-        p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_edit?id=" + data_[i]["id"] + '"><i class="fas fa-pen"></i></a></td>';
-        p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_remove?id=" + data_[i]["id"] + '"><i class="fas fa-trash"></i></a></td>';
+        p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_detail/" + data_[i]["id"] + '"><i class="fas fa-eye"></i></a></td>';
+        p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_edit/" + data_[i]["id"] + '"><i class="fas fa-pen"></i></a></td>';
+        p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_remove/" + data_[i]["id"] + '"><i class="fas fa-trash"></i></a></td>';
         p_list += (`</tr>`);
       }
     }
