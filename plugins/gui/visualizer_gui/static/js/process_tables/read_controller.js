d7a989b2e3a74bf3f05458f70a8570f9c2d4a485
Harvey Bastidas <harveybc@ingeni-us.com>
2024-04-03 18:44:58 -0500

    Update read_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/read_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/read_controller.js
@@ -18,7 +18,7 @@ export class ReadController {
         let url = '/' + this.process.name + '/' + this.table.name + '/detail/' + reg_id;
         axios.get(url, { params: {} })
             .then((response) => {
-              let data_ = JSON.parse(response.data);
+              let data_ = response.data;
               print(data_);
               let table = document.getElementById('detail_table');
               var p_list = "";
