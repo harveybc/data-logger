d71dea1c3d11698905fde4adf3d5f33750bb12c6
Harvey Bastidas <harveybc@ingeni-us.com>
2024-04-03 18:05:23 -0500

    Update read.py

--- plugins/gui/visualizer_gui/blueprints/process_tables/read.py
+++ plugins/gui/visualizer_gui/blueprints/process_tables/read.py
@@ -11,7 +11,7 @@ def read_data(db, Base, process, table, id):
         table_name = table['name']
         print("table['name'] : ", table['name'])
         res = db.session.query(Base.classes[table_name]).filter(Base.classes[table_name].id == id).one()
-        return json.dumps(res, default=str)
+        return json.dumps(as_dict(res), default=str)
     except SQLAlchemyError as e:
         error = str(e)
         print("SQLAlchemyError : " , error)
