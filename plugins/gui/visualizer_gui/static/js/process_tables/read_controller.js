4cb3af5090764b08ef8153ed55ecb73177ef925d
Harvey Bastidas <harveybc@ingeni-us.com>
2024-04-04 23:19:58 -0500

    Update read_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/read_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/read_controller.js
@@ -48,40 +48,12 @@ export class ReadController {
     }
     return p_list;
   }  
-  // updates the list of the table in the index view
-  // params: start: the starting index of the data_ array
-  //         num_rows: the number of rows to be added to the table
-  //         data_: the data array
-  detail_table_update(data_) {
-    var p_list = "";
-    // verify if is defined p_config_gui.gui_plugin_config[table['name']].index.columns_visible and show those columns in the row
-    if (((this.p_config_gui.gui_plugin_config[table['name']]) && (this.p_config_gui.gui_plugin_config[table['name']].index)) && (this.p_config_gui.gui_plugin_config[table['name']].index.columns_visible)) {
-      // for each table['columns'] create a new row of the table in the html element with id index_list
-      p_list += (`<tr>`);
-      for (let j = 0; j < this.p_config_gui.gui_plugin_config[table['name']].index.columns_visible.length; j++) {
-        let col = this.p_config_gui.gui_plugin_config[table['name']].index.columns_visible[j];
-        p_list += (`<td>${data_[i][col]}</td>`);
-      }
-      //p_list += "<td>View</td><td>Edit</td><td>Delete</td>"
-      p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_detail/" + data_[i]["id"] + '"><i class="fas fa-eye"></i></a></td>';
-      p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_edit/" + data_[i]["id"] + '"><i class="fas fa-pen"></i></a></td>';
-      p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_remove/" + data_[i]["id"] + '"><i class="fas fa-trash"></i></a></td>';
-      p_list += (`</tr>`);
-    }
-    // else show all columns in the table.columns list
-    else {
-      // for each table['columns'] create a new row of the table in the html element with id index_list
-      p_list += (`<tr>`);
-      p_list += (`<td>${data_[i]['id']}</td>`);
-      for (let j = 0; j < table.columns.length; j++) {
-        let col = table.columns[j].name;
-        p_list += (`<td>${data_[i][col]}</td>`);
-      }
-      p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_detail/" + data_[i]["id"] + '"><i class="fas fa-eye"></i></a></td>';
-      p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_edit/" + data_[i]["id"] + '"><i class="fas fa-pen"></i></a></td>';
-      p_list += '<td><a href="/' + this.process.name + "/" + this.table.name + "/view_remove/" + data_[i]["id"] + '"><i class="fas fa-trash"></i></a></td>';
-      p_list += (`</tr>`);
-    }
+  // returns an axios instance with configured basic authentication
+  // TODO: change to use current user
+  axios_auth_instance() {
+    let axios_instance = this.axiosBasicAuth("test", "pass");
+    return axios_instance;
   }
+  
 }
 
