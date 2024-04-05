d1b879735df3f5d7bee5842c129ec3c17937bcea
Harvey Bastidas <harveybc@ingeni-us.com>
2024-04-04 23:21:05 -0500

    Update read_controller.js

--- plugins/gui/visualizer_gui/static/js/process_tables/read_controller.js
+++ plugins/gui/visualizer_gui/static/js/process_tables/read_controller.js
@@ -48,6 +48,18 @@ export class ReadController {
     }
     return p_list;
   }  
+
+  // returns an axios instance for basic authentication
+  axiosBasicAuth(username, password) {
+    let buffer_auth = buffer.Buffer.from(username + ':' + password);
+    let b64 = buffer_auth.toString('base64');
+    return axios.create({
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Basic ${b64}`,
+      }
+    });
+  }
   // returns an axios instance with configured basic authentication
   // TODO: change to use current user
   axios_auth_instance() {
