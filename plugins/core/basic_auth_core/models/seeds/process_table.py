e12852f205aa0ab0529a3d5f69ea7bcc8649de61
Harvey Bastidas <harveybc@ingeni-us.com>
2024-05-09 23:50:26 -0500

    update

--- plugins/core/basic_auth_core/models/seeds/process_table.py
+++ plugins/core/basic_auth_core/models/seeds/process_table.py
@@ -21,45 +21,177 @@ def seed(app, db, table_name):
             table_name = sanitize_str(table_name, 256)
             Base.prepare(db.engine, reflect=False)
             # table base class
-            table_base = eval("Base.classes." + table_name)
+            table_base = Base.classes[table_name]
             # perform query
             try:
                 rows=[]
+                
+                columnas =  [
+                            {
+                                "name": "csv_file",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "save_encoder",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "save_decoder",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "load_encoder",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "load_decoder",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "evaluate_encoder",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "evaluate_decoder",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "encoder_plugin",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "decoder_plugin",
+                                "col_type": "String" 
+                            },
+                            {
+                                "name": "window_size",
+                                "col_type": "Integer" 
+                            },
+                            {
+                                "name": "max_error",
+                                "col_type": "Float" 
+                            },
+                            {
+                                "name" : "initial_size",
+                                "col_type" : "Integer"
+                            },
+                            {
+                                "name": "step_size",
+                                "col_type": "Integer"
+                            },
+                            {
+                                "name": "remote_log",
+                                "col_type": "String"
+                            },
+                            {
+                                "name": "quiet_mode",
+                                "col_type": "Integer"
+                            },
+                            {
+                                "name": "active",
+                                "col_type": "Boolean"
+                            }
+                        ]
+                #append all the columnsof the table 
                 if table_name == "fe_config":
-                    rows.append(table_base(initial_capital=10000, active=True))
-                    rows.append(table_base(initial_capital=1000, active=True))
-                    rows.append(table_base(initial_capital=9000, active=False))
+                    rows.append(table_base(csv_file="EURUSD_5m_mar2014_mar2024_preprocessed.csv", 
+                                            save_encoder="EURUSD_5m_mar2014_mar2024_encoder.h5", 
+                                            save_decoder="EURUSD_5m_mar2014_mar2024_decoder.h5", 
+                                            load_encoder="EURUSD_5m_mar2014_mar2024_encoder.h5", 
+                                            load_decoder="EURUSD_5m_mar2014_mar2024_decoder.h5", 
+                                            evaluate_encoder="EURUSD_5m_mar2014_mar2024_encoder.h5", 
+                                            evaluate_decoder="EURUSD_5m_mar2014_mar2024_decoder.h5", 
+                                            encoder_plugin="cnn", 
+                                            decoder_plugin="cnn", 
+                                            window_size=512, 
+                                            max_error=0.1, 
+                                            initial_size=256, 
+                                            step_size=16, 
+                                            remote_log="http://localhost:60500/feature_extractor/fe_training_error/1", 
+                                            quiet_mode=0, 
+                                            active=True))
+                    rows.append(table_base(csv_file="EURUSD_15m_mar2014_mar2024_preprocessed.csv",
+                                            save_encoder="EURUSD_15m_mar2014_mar2024_encoder.h5",
+                                            save_decoder="EURUSD_15m_mar2014_mar2024_decoder.h5",
+                                            load_encoder="EURUSD_15m_mar2014_mar2024_encoder.h5",
+                                            load_decoder="EURUSD_15m_mar2014_mar2024_decoder.h5",
+                                            evaluate_encoder="EURUSD_15m_mar2014_mar2024_encoder.h5",
+                                            evaluate_decoder="EURUSD_15m_mar2014_mar2024_decoder.h5",
+                                            encoder_plugin="cnn",
+                                            decoder_plugin="cnn",
+                                            window_size=256,
+                                            max_error=0.1,
+                                            initial_size=128,
+                                            step_size=8,
+                                            remote_log="http://localhost:60500/feature_extractor/fe_training_error/2",
+                                            quiet_mode=0,
+                                            active=True))
+                    rows.append(table_base(csv_file="EURUSD_1h_mar2014_mar2024_preprocessed.csv", 
+                                            save_encoder="EURUSD_1h_mar2014_mar2024_encoder.h5", 
+                                            save_decoder="EURUSD_1h_mar2014_mar2024_decoder.h5", 
+                                            load_encoder="EURUSD_1h_mar2014_mar2024_encoder.h5", 
+                                            load_decoder="EURUSD_1h_mar2014_mar2024_decoder.h5", 
+                                            evaluate_encoder="EURUSD_1h_mar2014_mar2024_encoder.h5", 
+                                            evaluate_decoder="EURUSD_1h_mar2014_mar2024_decoder.h5", 
+                                            encoder_plugin="cnn", 
+                                            decoder_plugin="cnn", 
+                                            window_size=128, 
+                                            max_error=0.1, 
+                                            initial_size=64, 
+                                            step_size=4, 
+                                            remote_log="http://localhost:60500/feature_extractor/fe_training_error/3", 
+                                            quiet_mode=0, 
+                                            active=True))
+                    rows.append(table_base(csv_file="EURUSD_4h_mar2014_mar2024_preprocessed.csv",
+                                               save_encoder="EURUSD_4h_mar2014_mar2024_encoder.h5",
+                                                save_decoder="EURUSD_4h_mar2014_mar2024_decoder.h5",
+                                                load_encoder="EURUSD_4h_mar2014_mar2024_encoder.h5",
+                                                load_decoder="EURUSD_4h_mar2014_mar2024_decoder.h5",
+                                                evaluate_encoder="EURUSD_4h_mar2014_mar2024_encoder.h5",
+                                                evaluate_decoder="EURUSD_4h_mar2014_mar2024_decoder.h5",
+                                                encoder_plugin="cnn",
+                                                decoder_plugin="cnn",
+                                                window_size=64,
+                                                max_error=0.1,
+                                                initial_size=32,
+                                                step_size=2,
+                                                remote_log="http://localhost:60500/feature_extractor/fe_training_error/4",
+                                                quiet_mode=0,
+                                                active=True))                            
+                    rows.append(table_base(csv_file="EURUSD_1d_mar2014_mar2024_preprocessed.csv",
+                                               save_encoder="EURUSD_4h_mar2014_mar2024_encoder.h5",
+                                                save_decoder="EURUSD_4h_mar2014_mar2024_decoder.h5",
+                                                load_encoder="EURUSD_4h_mar2014_mar2024_encoder.h5",
+                                                load_decoder="EURUSD_4h_mar2014_mar2024_decoder.h5",
+                                                evaluate_encoder="EURUSD_4h_mar2014_mar2024_encoder.h5",
+                                                evaluate_decoder="EURUSD_4h_mar2014_mar2024_decoder.h5",
+                                                encoder_plugin="cnn",
+                                                decoder_plugin="cnn",
+                                                window_size=32,
+                                                max_error=0.1,
+                                                initial_size=16,
+                                                step_size=1,
+                                                remote_log="http://localhost:60500/feature_extractor/fe_training_error/5",
+                                                quiet_mode=0,
+                                                active=True))                            
+                    # add all the rows to the session
                     for row in rows:
                         db.session.add(row)
+                    # commit the session to the database
                     db.session.commit()
                     _logger.info("fe_config table seeded") 
-                elif table_name == "gym_fx_data":
-                    rows.append(table_base(score=0.11, score_v=0.09, config_id=1))
-                    rows.append(table_base(score=0.1, score_v=0.09, config_id=2))
-                    rows.append(table_base(score=0.14, score_v=0.13, config_id=1))
-                    rows.append(table_base(score=0.13, score_v=0.12, config_id=3))
-                    for row in rows:
-                        db.session.add(row)
-                    db.session.commit()
-                    _logger.info("gym_fx_data table seeded") 
                 elif table_name == "fe_training_error":
-                    # TODO: add reward, num_closes
-                    rows.append(table_base(balance=10000, equity=10000, order_status= 0, reward=0, num_closes=0, config_id=3, tick_timestamp=0))
-                    rows.append(table_base(balance=10000, equity=13000, order_status= 1, reward=0, num_closes=0 ,config_id=3, tick_timestamp=1))
-                    rows.append(table_base(balance=12000, equity=12000, order_status= 0, reward=2000, num_closes=1 ,config_id=3, tick_timestamp=2))
-                    rows.append(table_base(balance=12000, equity=7000, order_status= -1, reward=0, num_closes=1 ,config_id=3, tick_timestamp=3))
-                    rows.append(table_base(balance=8000, equity=6000, order_status= 0, reward=-2000, num_closes=2,config_id=3, tick_timestamp=4))
-                    rows.append(table_base(balance=9000, equity=8000, order_status= -1, reward=0, num_closes=2 ,config_id=3, tick_timestamp=5))
-                    rows.append(table_base(balance=9000, equity=9000, order_status= 0, reward=0, num_closes=3 ,config_id=3, tick_timestamp=6))
-                    rows.append(table_base(balance=9000, equity=11000, order_status= 1, reward=0, num_closes=3 ,config_id=3, tick_timestamp=7))
-                    rows.append(table_base(balance=13000, equity=13000, order_status= 0 , reward=4000, num_closes=4,config_id=3, tick_timestamp=8))
-                    rows.append(table_base(balance=13000, equity=12000, order_status= 1, reward=0, num_closes=4 ,config_id=3, tick_timestamp=9))
-                    rows.append(table_base(balance=11000, equity=11000, order_status= 0 , reward=-2000, num_closes=5,config_id=3, tick_timestamp=10))
-                    rows.append(table_base(balance=11000, equity=13000, order_status= 1, reward=0, num_closes=5 ,config_id=3, tick_timestamp=11))
+                    rows.append(table_base(mse=0.9, interface_size=256, config_id=1))
+                    rows.append(table_base(mse=0.9, interface_size=128, config_id=2))
+                    rows.append(table_base(mse=0.9, interface_size=64, config_id=3))
+                    rows.append(table_base(mse=0.9, interface_size=32, config_id=4))
+                    rows.append(table_base(mse=0.9, interface_size=16, config_id=5))
+                    # add all the rows to the session
                     for row in rows:
                         db.session.add(row)
                     db.session.commit()
-                    _logger.info("gym_fx_validation_plot table seeded") 
+                    _logger.info("fe_training_error table seeded") 
+            
             except SQLAlchemyError as e:
                 error = str(e)
                 print("Error : " , error)
