a9221fd0e5754808167991d891c4eb456fee532f
Harvey Bastidas <harveybc@ingeni-us.com>
2024-07-30 22:41:01 -0500

    Update evaluator.py

--- plugins/gui/visualizer_gui/blueprints/evaluator.py
+++ plugins/gui/visualizer_gui/blueprints/evaluator.py
@@ -24,11 +24,12 @@ def new_bp(plugin_folder, core_ep, store_ep, db, Base):
         """Create a new evaluation request"""
         print("Headers: ", request.headers)
         print("Body: ", request.get_data(as_text=True))
-        #print("JSON: ", request.get_json())
+        print("JSON: ", request.get_json())
         print("Args: ", request.args)
         print("Form: ", request.form)
         print("Files: ", request.files)
         try:
+            print("JSON: ", request.get_json())
             content = request.json
             required_fields = ['client_id', 'data', 'window_size']
 
