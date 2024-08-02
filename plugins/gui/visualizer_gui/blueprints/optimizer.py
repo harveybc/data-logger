d63b2306c472a189f11dcf65ab6e9e327a0ee0fd
Harvey Bastidas <harveybc@ingeni-us.com>
2024-08-02 18:59:12 -0500

    Update optimizer.py

--- plugins/gui/visualizer_gui/blueprints/optimizer.py
+++ plugins/gui/visualizer_gui/blueprints/optimizer.py
@@ -8,7 +8,7 @@ import json
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-def new_bp(plugin_folder, core_ep, store_ep, db, Base):
+def new_bp_optimizer(db, Base):
     # Create a new Blueprint
     bp_optimizer = Blueprint("bp_optimizer", __name__)
 
@@ -25,27 +25,23 @@ def new_bp(plugin_folder, core_ep, store_ep, db, Base):
     def fetch_optimum():
         try:
             content = request.json
-            required_fields = ['current_optimum_hash']
+            required_fields = ['optimization_id', 'current_optimum_hash']
 
             # Validate input data
             for field in required_fields:
                 if field not in content:
                     return jsonify({"error": f"Missing field: {field}"}), 400
 
+            optimization_id = content['optimization_id']
             current_optimum_hash = content['current_optimum_hash']
 
-            latest_optimization = db.session.query(Optimizations).filter_by(status='active').order_by(Optimizations.id.desc()).first()
-            
-            if not latest_optimization:
-                return jsonify({"message": "No optimizations available"}), 204
-
-            if latest_optimization.last_optimum_hash == current_optimum_hash:
-                return jsonify({"message": "No new optimum reported"}), 200
-
-            latest_optimum = db.session.query(Optimae).filter_by(optimum_hash=latest_optimization.last_optimum_hash).first()
+            latest_optimum = db.session.query(Optimae).filter_by(optimizer_id=optimization_id).order_by(Optimae.id.desc()).first()
 
             if not latest_optimum:
-                return jsonify({"error": "Optimum not found"}), 404
+                return jsonify({"message": "No optimizations available for the given ID"}), 204
+
+            if latest_optimum.optimum_hash == current_optimum_hash:
+                return jsonify({"message": "No new optimum reported"}), 200
 
             return jsonify({
                 "optimum": latest_optimum.optimum,
